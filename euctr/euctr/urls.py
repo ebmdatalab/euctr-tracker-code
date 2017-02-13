164157c53a4fbc19293e9ee074d6befbce8c7d58
Francis Irving <francis@flourish.org>
2017-02-13 09:11:22 +0000

    Add frontend app with basic view

--- euctr/euctr/urls.py
+++ euctr/euctr/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+import frontend.views
+
 urlpatterns = [
+    url(r'^$', frontend.views.index),
     url(r'^admin/', admin.site.urls),
 ]
