5cbe01b1dd566344a03d9e6d297fec81340c058d
Francis Irving <francis@flourish.org>
2017-02-13 17:59:27 +0000

    Move data directory to top level

--- euctr/frontend/models.py
+++ euctr/frontend/models.py
@@ -4,9 +4,9 @@ import json
 
 
 def get_headlines():
-    data = json.load(open('data/headline.json'))
+    data = json.load(open('../data/headline.json'))
     return data
 
 def get_table4():
-    data = json.load(open('data/table4.json'))
+    data = json.load(open('../data/table4.json'))
     return data
