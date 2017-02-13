d9ecb0100a88fcdd3a554fbf2e82ff32aeb58b98
Francis Irving <francis@flourish.org>
2017-02-13 14:51:59 +0000

    PEP 8 line spacing

--- euctr/frontend/models.py
+++ euctr/frontend/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 import json
 
+
 def get_headlines():
     data = json.load(open('data/headline.json'))
     return data
-
--- euctr/frontend/views.py
+++ euctr/frontend/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from . import models
 
+
 def index(request):
     headlines = models.get_headlines()
     return render(request, "index.html", context=headlines)
