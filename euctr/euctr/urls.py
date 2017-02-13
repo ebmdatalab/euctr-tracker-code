f1e6fdaa10640d9ce732f9730543ba1f023efa28
Francis Irving <francis@flourish.org>
2017-02-13 14:16:05 +0000

    Add "Journal" bootswatch theme, and basic pages

--- euctr/euctr/urls.py
+++ euctr/euctr/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 import frontend.views
 
 urlpatterns = [
     url(r'^$', frontend.views.index),
+    url(r'^about$', TemplateView.as_view(template_name="about.html")),
     url(r'^admin/', admin.site.urls),
 ]
