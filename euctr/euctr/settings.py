00d2bb7d1090a7ecdc91a36124102175c99ec273
Francis Irving <francis@flourish.org>
2017-02-13 10:16:07 +0000

    Add basic use of templates

--- euctr/euctr/settings.py
+++ euctr/euctr/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'euctr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- euctr/frontend/views.py
+++ euctr/frontend/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
 
 def index(request):
-        return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, "index.html")
