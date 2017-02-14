8013a14c213596a4a08bb504b9c47377d2e0039b
Francis Irving <francis@flourish.org>
2017-02-14 16:59:52 +0000

    Create table 4 JSON from spreadsheet

--- euctr/euctr/management/commands/loadtrialsdata.py
+++ euctr/euctr/management/commands/loadtrialsdata.py
@@ -1,12 +1,21 @@
 import pandas
 import sys
 import json
+import numpy as np
 
 from django.core.management.base import BaseCommand, CommandError
 
 SOURCE_XLS_FILE = '../data/export-from-stata-2016-02-13.xls'
 OUTPUT_HEADLINE_FILE = '../data/headline.json'
+OUTPUT_TABLE4_FILE = '../data/table4.json'
+TABLE_4_THRESHOLD = 50
 
+def yes_no_to_number(x):
+    if x == 'yes':
+        return 1
+    if x == 'no':
+        return 0
+    assert False
 
 class Command(BaseCommand):
     help = 'Loads in data'
@@ -28,4 +37,23 @@ class Command(BaseCommand):
 
         with open(OUTPUT_HEADLINE_FILE, 'w') as outfile:
             json.dump(headline, outfile)
+
+        table4_trials = due_trials[due_trials['count'] >= TABLE_4_THRESHOLD]
+        table4_trials = table4_trials[
+            ['normalized_name_of_sponsor', 'results', 'are_results_due']
+        ]
+        table4_trials['results'] = table4_trials['results'].map(yes_no_to_number)
+        by_sponsor = table4_trials.groupby('normalized_name_of_sponsor')
+        table4 = by_sponsor.sum()
+        table4.reset_index(level=0, inplace=True)
+        table4.rename(columns={
+            'normalized_name_of_sponsor': 'sponsor_name',
+            'results': 'total_reported',
+            'are_results_due': 'total_due'
+        }, inplace=True)
+        table4['percent_reported'] = np.round(table4['total_reported'] / table4['total_due'] * 100, 1)
+
+        pandas.set_option('display.width', 200)
+        print(table4)
+        table4.to_json(OUTPUT_TABLE4_FILE, orient='records')
 
