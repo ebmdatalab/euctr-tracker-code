82201be1bee6b9c0d49fcfc0b13ab3ebbbedbf4d
Francis Irving <francis@flourish.org>
2017-02-14 17:21:27 +0000

    Fix to use quartiles for which items in table 4

--- euctr/static/js/site.js
+++ euctr/static/js/site.js
@@ -1,3 +1,7 @@
 $(document).ready( function () {
-    $('#table4_table').DataTable();
+    var t = $('#table4_table').DataTable({
+        "order": [[ 3, "desc" ]],
+        //order": [[ 3, "desc" ], [2, "desc"], [0, "asc"]], // reproduce order in paper
+		"paging": false
+    });
 } );
