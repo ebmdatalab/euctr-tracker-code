c35771417a5fc614ce19b82f85622c0918d41198
Francis Irving <francis@flourish.org>
2017-02-14 18:36:28 +0000

    Make default sort order descending for numeric columns

--- euctr/static/js/site.js
+++ euctr/static/js/site.js
@@ -2,6 +2,12 @@ $(document).ready( function () {
     var t = $('#table4_table').DataTable({
         "order": [[ 3, "desc" ]],
         //order": [[ 3, "desc" ], [2, "desc"], [0, "asc"]], // reproduce order in paper
-		"paging": false
+	"paging": false,
+	"aoColumns": [
+	    { "orderSequence": [ "asc", "desc" ] },
+	    { "orderSequence": [ "desc", "asc" ] },
+	    { "orderSequence": [ "desc", "asc" ] },
+	    { "orderSequence": [ "desc", "asc" ] }
+	]
     });
 } );
