a182850bbdcf6a1460af80f443912b4867e35907
Francis Irving <francis@flourish.org>
2017-05-09 14:02:37 +0100

    Fix threshold date to be dynamic
    1 year and 3 weeks before start of scrape.

--- euctr/euctr/management/commands/get_trials_from_db.py
+++ euctr/euctr/management/commands/get_trials_from_db.py
@@ -3,6 +3,7 @@ import os
 import sys
 import psycopg2
 import csv
+import datetime
 
 from atomicwrites import atomic_write
 
@@ -20,11 +21,26 @@ class Command(BaseCommand):
         conn = psycopg2.connect(opentrials_db)
         cur = conn.cursor()
 
+        # Find out the earliest date of current scrape.
+        # The "count(*) > 100" part can be removed when
+        # https://github.com/opentrials/opentrials/issues/821 is fixed
+        cur.execute("""select count(*) as c, date(meta_updated) as d from euctr
+                group by d
+                having count(*) > 100
+                order by d limit 1""")
+        scrape_date = cur.fetchone()[1]
+
+        # Date for reporting to be due has cutoff is 1 year (365 days) (by law,
+        # trials must report a year after finishing) plus 3 weeks (21 days)
+        # allowance (it takes that long for submissions to enter register)
+        due_date_cutoff = scrape_date - datetime.timedelta(days=365 + 21)
+
         query = open("euctr/management/commands/opentrials-to-csv.sql").read()
-        params = { 'due_date_cutoff': '2016-04-10' }
+        params = { 'due_date_cutoff': due_date_cutoff }
         cur.execute(query, params)
 
         with atomic_write(TRIALS_CSV_FILE, overwrite=True) as f:
             writer = csv.writer(f, lineterminator="\n")
             writer.writerow([i[0] for i in cur.description])
             writer.writerows(cur)
+
