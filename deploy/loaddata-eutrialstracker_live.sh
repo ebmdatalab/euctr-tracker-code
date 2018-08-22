1e71cc63d9887c2773b26a916569d829906efc0a
Seb Bacon <seb.bacon@gmail.com>
2018-08-22 15:09:54 +0100

    Fix activation of django environment

--- deploy/loaddata-eutrialstracker_live.sh
+++ deploy/loaddata-eutrialstracker_live.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 export GIT_SSH_COMMAND='ssh -i /var/www/eutrialstracker_live/ssh-keys/id_rsa_eutrialtracker_data'
+. /etc/profile.d/eutrialstracker_live.sh
 
 # get updates to sponsor name normalization spreadsheet in particular
 cd /var/www/eutrialstracker_live/euctr-tracker-data
 git pull -q
 chown -R www-data:www-data .
-
-# activate django environment
-. /etc/profile.d/eutrialstracker_live.sh
-. venv/bin/activate
-cd /var/www/eutrialstracker_live/euctr-tracker-data
 
 # Only proceed if there are uncommitted files. These should only exist
 # as a result of crawl-and-dump-eutrialstracker_live.sh having been
@@ -19,6 +15,8 @@ git add -A
 changes=$(git diff-index HEAD)
 if [[ ! -z "$changes" ]]; then
     # There are uncommitted changes: turn CSV into JSON
+    # activate django environment
+    . /var/www/eutrialstracker_live/venv/bin/activate
     json_update_unfinished=$(./manage.py update_trials_json)
 
     # The management command prints output to STDOUT (and emails
