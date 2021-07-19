fa82354f7fd018f193a117f63cc2617d7e135a3f
George Hickman <george@ghickman.co.uk>
2021-07-19 14:02:00 +0100

    Bump django to 2.2.24

--- euctr/frontend/tests/databaseless_runner.py
+++ euctr/frontend/tests/databaseless_runner.py
@@ -5,7 +5,7 @@ from django.test.runner import DiscoverRunner
 class DatabaselessTestRunner(DiscoverRunner):
     """A test suite runner that does not set up and tear down a database."""
 
-    def setup_databases(self):
+    def setup_databases(self, *args, **kwargs):
         """Overrides DjangoTestSuiteRunner"""
         pass
 
