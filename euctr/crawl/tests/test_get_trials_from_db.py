9921e941f746c8fdbec171e02296230412d99e92
Tom Ward <tomward@fmail.co.uk>
2021-08-19 15:00:13 +0100

    Refactor and write some basic tests for sufficiently_old
    * it turned out that the flaw was not in this logic, but I wrote these tests whilst attempting to understand the problem

--- euctr/crawl/tests/test_get_trials_from_db.py
+++ euctr/crawl/tests/test_get_trials_from_db.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+import datetime
 import os
 import pandas as pd
 import numpy as np
@@ -16,3 +17,43 @@ class GetTrialsFromDbTestCase(TestCase):
         # use the old 0.19.2 pandas undocumented function
         # rather than the 0.20 public function with a similar name
         pdt.assert_frame_equal(df_output, df_test_output)
+
+    def test_calculate_sufficiently_old(self):
+        scrape_dates = [
+            datetime.date(2021, 1, 1),
+            datetime.date(2020, 12, 1),
+            datetime.date(2020, 11, 1),
+        ]
+
+        last_scrape_date = scrape_dates[0]
+        verbosity = 0
+        sufficiently_old = euctr.management.commands.get_trials_from_db.calculate_sufficiently_old(scrape_dates, last_scrape_date, verbosity)
+
+        assert(sufficiently_old == datetime.date(2020, 11, 1))
+
+    def test_calculate_sufficiently_old_60_days(self):
+        scrape_dates = [
+            datetime.date(2021, 1, 1),
+            datetime.date(2020, 12, 1),
+            datetime.date(2020, 11, 6),
+            datetime.date(2020, 11, 5),
+            datetime.date(2020, 11, 4),
+            datetime.date(2020, 11, 3),
+            datetime.date(2020, 11, 2),
+            datetime.date(2020, 11, 1),
+        ]
+
+        last_scrape_date = scrape_dates[0]
+        verbosity = 0
+        sufficiently_old = euctr.management.commands.get_trials_from_db.calculate_sufficiently_old(scrape_dates, last_scrape_date, verbosity)
+        assert(sufficiently_old == datetime.date(2020, 11, 2))
+
+    def test_calculate_sufficiently_insufficient_count(self):
+        scrape_dates = [
+            datetime.date(2021, 1, 1),
+        ]
+
+        last_scrape_date = scrape_dates[0]
+        verbosity = 0
+        sufficiently_old = euctr.management.commands.get_trials_from_db.calculate_sufficiently_old(scrape_dates, last_scrape_date, verbosity)
+        assert(sufficiently_old == datetime.date(2020, 12, 31))
